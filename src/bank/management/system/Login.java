7f4e0613be0c19e1d185b78045a03ec7cc4677c8
Nikhilkumar2311 <120698682+Nikhilkumar2311@users.noreply.github.com>
2024-02-18 23:21:00 +0530

    Login Page

---
+++ src/bank/management/system/Login.java
@@ -0,0 +1,112 @@
+package bank.management.system;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Login extends JFrame implements ActionListener {
+    JTextField textField2;
+    JPasswordField passwordField3;
+    JButton button1, button2, button3;
+    Login(){
+        super("Bank Management System");
+
+        addScaledImageIcon("icon/bank.png", 350, 10, 100, 100);
+        addScaledImageIcon("icon/card.png", 630, 350, 100, 100);
+
+        createLabel("WELCOME TO ATM", "AvantGarde", Font.BOLD, 38, Color.WHITE, 230, 125, 450, 40);
+        createLabel("Card No:", "Ralway", Font.BOLD, 28, Color.WHITE, 150, 190, 375, 30);
+
+        textField2 = new JTextField(15);
+        textField2.setBounds(325, 190, 230, 30);
+        textField2.setFont(new Font("Arial", Font.BOLD, 14));
+        add(textField2);
+
+        createLabel("PIN:", "Ralway", Font.BOLD, 28, Color.WHITE, 150, 250, 375, 30);
+
+        passwordField3 = new JPasswordField(15);
+        passwordField3.setBounds(325, 250, 230, 30);
+        passwordField3.setFont(new Font("Arial", Font.BOLD, 14));
+        add(passwordField3);
+
+
+        addButton("SIGN IN", 300, 300, 100, 30, 1);
+        addButton("CLEAR", 430, 300, 100, 30, 2);
+        addButton("SIGN UP", 300, 350, 230, 30, 3);
+
+        addScaledImageIcon("icon/backbg.png", 0, 0, 850, 480);
+
+        setLayout(null);
+        setSize(850, 480);
+        setLocation(250, 100);
+        setVisible(true);
+    }
+
+    private void addScaledImageIcon(String imagePath, int x, int y, int width, int height) {
+        ImageIcon icon = new ImageIcon(ClassLoader.getSystemResource(imagePath));
+        Image scaledImage = icon.getImage().getScaledInstance(width, height, Image.SCALE_DEFAULT);
+        ImageIcon scaledIcon = new ImageIcon(scaledImage);
+        JLabel label = new JLabel(scaledIcon);
+        label.setBounds(x, y, width, height);
+        add(label);
+    }
+
+    private JLabel createLabel(String text, String fontName, int fontStyle, int fontSize, Color color, int x, int y, int width, int height) {
+        JLabel label = new JLabel(text);
+        label.setFont(new Font(fontName, fontStyle, fontSize));
+        label.setForeground(color);
+        label.setBounds(x, y, width, height);
+        add(label);
+        return label;
+    }
+
+    private void addButton(String text, int x, int y, int width, int height, int buttonNumber) {
+        JButton button = new JButton(text);
+        button.setFont(new Font("Arial", Font.BOLD, 14));
+        button.setForeground(Color.WHITE);
+        button.setBackground(Color.BLACK);
+        button.setBounds(x, y, width, height);
+        button.addActionListener(this);
+        add(button);
+
+        switch (buttonNumber) {
+            case 1:
+                button1 = button;
+                break;
+            case 2:
+                button2 = button;
+                break;
+            case 3:
+                button3 = button;
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid button number");
+        }
+    }
+
+    // To add the action on button we implemented the ActionListener
+    // here e will tell which button is clicked
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        try{
+            if (e.getSource() == button1){
+
+            }
+            else if (e.getSource() == button2) {
+                textField2.setText("");
+                passwordField3.setText("");
+            }
+            else if (e.getSource() == button3) {
+
+            }
+
+        } catch (Exception E){
+            E.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        new Login();
+    }
+}
